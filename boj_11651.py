2b659b4e7cc351ddf51ae95b766a86903d696644
RyuJiHyun <201802088@o.cnu.ac.kr>
2022-09-19 08:43:39 +0900

    Create boj_11651.py

---
+++ boj_11651.py
@@ -0,0 +1,29 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+arr = []
+for _ in range(n):
+    x, y = map(int, sys.stdin.readline().split())
+    arr.append((x, y))
+
+
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    pivot = arr[len(arr) // 2]
+    lesser_arr, equal_arr, greater_arr = [], [], []
+    for elem in arr:
+        if elem < pivot:
+            lesser_arr.append(elem)
+        elif elem > pivot:
+            greater_arr.append(elem)
+        else:
+            equal_arr.append(elem)
+
+    return quick_sort(lesser_arr) + equal_arr + quick_sort(greater_arr)
+
+arr = quick_sort(arr)
+for elem in arr:
+    print(elem[0], elem[1])
