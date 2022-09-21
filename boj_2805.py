ab4eb0d5369c2c0483b5ede9f2909f65667bcc29
RyuJiHyun <201802088@o.cnu.ac.kr>
2022-09-21 13:35:01 +0900

    Create boj_2805.py

---
+++ boj_2805.py
@@ -0,0 +1,21 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+trees = list(map(int, sys.stdin.readline().split()))
+
+start, end = 0, max(trees)
+
+while start <= end:
+    mid = (start + end) // 2
+    sum_of_cut_tree = 0
+
+    for tree in trees:
+        if tree > mid:
+            sum_of_cut_tree += (tree - mid)
+
+    if sum_of_cut_tree >= m:
+        start = mid + 1
+    else:
+        end = mid - 1
+
+print(end)
