73c038e831ea93ed7ef89c3bc0b76a949004e398
RyuJiHyun <201802088@o.cnu.ac.kr>
2022-09-30 20:01:32 +0900

    Create boj_9095.py

---
+++ boj_9095.py
@@ -0,0 +1,14 @@
+import sys
+
+n = int(sys.stdin.readline())
+dp = [0] * 11
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+
+for i in range(4, 11):
+    dp[i] = sum(dp[i - 3: i])
+
+for _ in range(n):
+    num = int(sys.stdin.readline())
+    print(dp[num])
